6bc432b995bea8ef1c87178b3f5796d9bcdb53dc
Kawamura Shintaro <kawasin73@gmail.com>
2018-06-11 22:57:43 +0900

    Fix asm

--- asm/main.go
+++ asm/main.go
@@ -66,23 +66,23 @@ func (a *Assembler) Assemble(r io.Reader, w io.Writer) error {
 		case "add":
 			err = a.r3Render(w, line, 0, 0)
 		case "addi":
-			err = a.imm2Render(w, line, 1)
+			err = a.r2immRender(w, line, 1)
 		case "sub":
 			err = a.r3Render(w, line, 0, 2)
 		case "lui":
-			err = a.imm1Render(w, line, 3)
+			err = a.r1immRender(w, line, 3)
 		case "and":
 			err = a.r3Render(w, line, 0, 8)
 		case "andi":
-			err = a.imm2Render(w, line, 4)
+			err = a.r2immRender(w, line, 4)
 		case "or":
 			err = a.r3Render(w, line, 0, 9)
 		case "ori":
-			err = a.imm2Render(w, line, 5)
+			err = a.r2immRender(w, line, 5)
 		case "xor":
 			err = a.r3Render(w, line, 0, 10)
 		case "xori":
-			err = a.imm2Render(w, line, 6)
+			err = a.r2immRender(w, line, 6)
 		case "nor":
 			err = a.r3Render(w, line, 0, 11)
 		case "sll":
@@ -127,12 +127,13 @@ func (a *Assembler) Assemble(r io.Reader, w io.Writer) error {
 	return nil
 }
 
+// op rd rs rt  -> op(6) rs(5) rt(5) rd(5) aux(11)
 func (a *Assembler) r3Render(w io.Writer, line []string, op, aux int) error {
 	if len(line) != 4 {
 		return errors.Errorf("too short elements. expected %v elements", 4)
 	} else if err := render(w, 6, op); err != nil {
 		return errors.Wrap(err, "render op")
-	} else if err = renderR(w, line[1:4]...); err != nil {
+	} else if err = renderR(w, line[2], line[3], line[1]); err != nil {
 		return errors.Wrap(err, "render r")
 	} else if err = render(w, 11, aux); err != nil {
 		return errors.Wrap(err, "render aux")
@@ -142,12 +143,13 @@ func (a *Assembler) r3Render(w io.Writer, line []string, op, aux int) error {
 	return nil
 }
 
+// op rd rs shift -> op(6) rs(5) "r0"(5) rd(5) shift(5) aux(6)
 func (a *Assembler) r2Render(w io.Writer, line []string, op, aux int) error {
 	if len(line) != 4 {
 		return errors.Errorf("too short elements. expected %v elements", 4)
 	} else if err := render(w, 6, op); err != nil {
 		return errors.Wrap(err, "render op")
-	} else if err = renderR(w, line[1], line[2], "r0"); err != nil {
+	} else if err = renderR(w, line[2], "r0", line[1]); err != nil {
 		return errors.Wrap(err, "render r")
 	} else if num, err := strconv.Atoi(line[3]); err != nil {
 		return errors.Wrap(err, "parse aux")
@@ -161,12 +163,13 @@ func (a *Assembler) r2Render(w io.Writer, line []string, op, aux int) error {
 	return nil
 }
 
+// op rs -> op(6) rs(5) "r0"(5) "r0"(5) aux(11)
 func (a *Assembler) r1Render(w io.Writer, line []string, op, aux int) error {
 	if len(line) != 2 {
 		return errors.Errorf("too short elements. expected %v elements", 2)
 	} else if err := render(w, 6, op); err != nil {
 		return errors.Wrap(err, "render op")
-	} else if err = renderR(w, "r0", line[1], "r0"); err != nil {
+	} else if err = renderR(w, line[1], "r0", "r0"); err != nil {
 		return errors.Wrap(err, "render r")
 	} else if err = render(w, 11, aux); err != nil {
 		return errors.Wrap(err, "render aux")
@@ -176,12 +179,13 @@ func (a *Assembler) r1Render(w io.Writer, line []string, op, aux int) error {
 	return nil
 }
 
-func (a *Assembler) imm2Render(w io.Writer, line []string, op int) error {
+// op rt rs imm -> op(6) rs(5) rt(5) imm(16)
+func (a *Assembler) r2immRender(w io.Writer, line []string, op int) error {
 	if len(line) != 4 {
 		return errors.Errorf("too short elements. expected %v elements", 4)
 	} else if err := render(w, 6, op); err != nil {
 		return errors.Wrap(err, "render op")
-	} else if err = renderR(w, line[1:3]...); err != nil {
+	} else if err = renderR(w, line[2], line[1]); err != nil {
 		return errors.Wrap(err, "render r")
 	} else if imm, err := strconv.Atoi(line[3]); err != nil {
 		return errors.Wrap(err, "parse imm")
@@ -193,12 +197,13 @@ func (a *Assembler) imm2Render(w io.Writer, line []string, op int) error {
 	return nil
 }
 
-func (a *Assembler) imm1Render(w io.Writer, line []string, op int) error {
+// op rt imm -> op(6) "r0"(5) rt(5) imm(16)
+func (a *Assembler) r1immRender(w io.Writer, line []string, op int) error {
 	if len(line) != 3 {
 		return errors.Errorf("too short elements. expected %v elements", 3)
 	} else if err := render(w, 6, op); err != nil {
 		return errors.Wrap(err, "render op")
-	} else if err = renderR(w, line[1], "r0"); err != nil {
+	} else if err = renderR(w, "r0", line[1]); err != nil {
 		return errors.Wrap(err, "render r")
 	} else if imm, err := strconv.Atoi(line[2]); err != nil {
 		return errors.Wrap(err, "parse imm")
@@ -210,6 +215,7 @@ func (a *Assembler) imm1Render(w io.Writer, line []string, op int) error {
 	return nil
 }
 
+// op rt dpl(rs) -> op(6) rs(5) rt(5) dpl(16)
 func (a *Assembler) dplRender(w io.Writer, line []string, op int) error {
 	if len(line) != 3 {
 		return errors.Errorf("too short elements. expected %v elements", 3)
@@ -217,7 +223,7 @@ func (a *Assembler) dplRender(w io.Writer, line []string, op int) error {
 		return errors.Wrap(err, "render op")
 	} else if base, dpl, err := parseDpl(line[2]); err != nil {
 		return errors.Wrap(err, "parse dpl")
-	} else if err = renderR(w, line[1], "r"+strconv.Itoa(base)); err != nil {
+	} else if err = renderR(w, "r"+strconv.Itoa(base), line[1]); err != nil {
 		return errors.Wrap(err, "render r")
 	} else if err = render(w, 16, dpl); err != nil {
 		return errors.Wrap(err, "render imm")
@@ -227,12 +233,13 @@ func (a *Assembler) dplRender(w io.Writer, line []string, op int) error {
 	return nil
 }
 
+// op rt rs label -> op(6) rs(5) rt(5) {label - i -1}(16)
 func (a *Assembler) r2labelRender(w io.Writer, line []string, op int) error {
 	if len(line) != 4 {
 		return errors.Errorf("too short elements. expected %v elements", 4)
 	} else if err := render(w, 6, op); err != nil {
 		return errors.Wrap(err, "render op")
-	} else if err = renderR(w, line[1:3]...); err != nil {
+	} else if err = renderR(w, line[2], line[1]); err != nil {
 		return errors.Wrap(err, "render r")
 	} else if label, ok := a.labels[line[3]]; !ok {
 		return fmt.Errorf("not found label %v", line[3])
@@ -244,6 +251,7 @@ func (a *Assembler) r2labelRender(w io.Writer, line []string, op int) error {
 	return nil
 }
 
+// op label -> op(6) label(26)
 func (a *Assembler) labelRender(w io.Writer, line []string, op int) error {
 	if len(line) != 2 {
 		return errors.Errorf("too short elements. expected %v elements", 2)
